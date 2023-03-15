f62267ecc296bf4b3bbec3167a0896ad9937fee7
mabidux <70872559+mabidux@users.noreply.github.com>
2023-03-15 16:49:59 -0400

    Fix

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ buttonsContainer.querySelectorAll('button').forEach(button => {
 });
 
 const misteriosMap = {
-    'Gozosos': ['Anunciação à Virgem Maria', 'Visita de Maria à sua prima Isabel', 'Nascimento de Nosso Senhor Jesus Cristo', 'Apresentação de Jesus ao templo', 'Jesus, que é perdido e reencontrado entre os doutores'],
+    'Gozosos': ['Anunciação à Virgem Maria', 'Visita de Maria à sua prima Isabel', 'Nascimento de Nosso Senhor Jesus Cristo', 'Apresentação de Jesus no templo', 'Jesus, que é perdido e reencontrado entre os doutores'],
     'Luminosos': ['Batismo de Jesus', 'Primeiro milagre de Jesus, transformando água em vinho', 'Anúncio do Reino de Deus e convite à conversão', 'Transfiguração de Jesus', 'Instituição da Eucaristia'],
     'Dolorosos': ['Agonia de Jesus no Horto das Oliveiras', 'A flagelação de Jesus Cristo', 'A coroação de espinhos', 'Jesus, que carrega a própria Cruz até o Calvário', 'A morte de Nosso Senhor Jesus Cristo'],
     'Gloriosos': ['Ressureição de Jesus', 'Ascenção de Jesus aos céus', 'Vinda do Espírito Santo sobre Maria e os apóstolos em Pentecostes', 'Assunção de Nossa Senhora', 'A coroação de Nossa Senhora como rainha dos céus e da Terra']
